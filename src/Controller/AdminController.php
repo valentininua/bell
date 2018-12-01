880763c2892e93a1fc689a75c1c5f762e6220f35
valentininua <380961726999@ya.ru>
2018-12-02 00:43:59 +0300

    current account

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use App\Entity\Report;
 class AdminController extends AbstractController
 {
     /**
@@ -15,13 +14,6 @@ class AdminController extends AbstractController
      */
     public function index()
     {
-
-
-        $product = $this->getDoctrine()
-            ->getRepository(Report::class)
-            ->find(1);
-var_dump($product);
-exit();
         $user = $this->getUser();
        // var_dump($user->balance);exit;
 
