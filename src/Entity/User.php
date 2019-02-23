d1c0ccfebbbbc0d7f4fea8f47b3a36a8bf54308f
valentininua <380961726999@ya.ru>
2019-02-23 23:58:03 +0300

    referralLink

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,10 +19,34 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     *@ORM\Column(type="integer", options={"default" : 0}, nullable=true)
+     */
+    protected $referralLink;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
     }
+
+    /**
+     * @return mixed
+     */
+    public function getReferralLink()
+    {
+        return $this->referralLink;
+    }
+
+    /**
+     * @param $referralLink
+     */
+    public function setReferralLink($referralLink): void
+    {
+        $this->referralLink = $referralLink;
+
+    }
+
+
 
 }
